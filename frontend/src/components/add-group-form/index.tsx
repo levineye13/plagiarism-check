cc0a74f720a231b004244d70b2c98f812f3db09a
Oleg Zharov <levineye@gmail.com>
2022-11-09 13:23:14 +0300

    feat: добавит компонент формы добавления групп

---
+++ frontend/src/components/add-group-form/index.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react';
+
+import Input from '../input';
+import Button from '../submit';
+import styles from './index.module.scss';
+
+const AddGroupForm: FC = () => {
+  return (
+    <form name="addGroup" className={styles.form}>
+      <Input
+        type="text"
+        required
+        placeholder="Добавить группу"
+        pattern="[А-ЯЁ]{4}\-\d\d\-\d\d"
+      />
+      <Button>Добавить</Button>
+    </form>
+  );
+};
+
+export default AddGroupForm;
