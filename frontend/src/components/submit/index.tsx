092b681e9e8c21dbcca61f097470c7a5bd1a41a4
Oleg Zharov <levineye@gmail.com>
2022-11-13 13:57:46 +0300

    feat: добавить колбэк для кнопки сабмита

--- frontend/src/components/submit/index.tsx
+++ frontend/src/components/submit/index.tsx
@@ -3,9 +3,19 @@ import React from 'react';
 import { FCWithChildren } from '../../utils/types';
 import styles from './index.module.scss';
 
-const Submit: FCWithChildren = ({ children }) => {
+interface ISubmit {
+  readonly onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  readonly style?: React.CSSProperties;
+}
+
+const Submit: FCWithChildren<ISubmit> = ({ children, onClick, style }) => {
   return (
-    <button type="button" className={styles.button}>
+    <button
+      type="button"
+      onClick={onClick}
+      className={styles.button}
+      style={style}
+    >
       {children}
     </button>
   );
