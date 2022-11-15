d8bb36d0ec6431f3c1b1134364ac9ebf7dd577cd
Oleg Zharov <levineye@gmail.com>
2022-11-15 13:12:42 +0300

    feat: доверстать страницу с добавлением ответа на задание

--- frontend/src/pages/answer/index.tsx
+++ frontend/src/pages/answer/index.tsx
@@ -1,19 +1,34 @@
 import React, { FC } from 'react';
 
 import Title from '../../components/section-title';
+import AssessmentForm from '../../components/assessment-form';
 import styles from './index.module.scss';
+
+const user = { role: 'teacher' };
 
 const Answer: FC = () => {
   return (
     <section className={styles.section}>
-      <Title style={{ marginBottom: 20 }}>Иванов Иван Иванович</Title>
+      <Title>Иванов Иван Иванович</Title>
       <p className={styles.subtitle}>Ответ на задание</p>
-      <code className={styles.answer}>
-        FOR J=0 TO N-1 STEP 1
-          F=0
-          MIN=J
-        FOR I=J TO N-1-J STEP 1 
-      </code>
+      <pre className={styles.answer}>
+        <code className={styles.code}>
+          {`
+            function bubbleSortConcept1(arr) {
+              for (let j = arr.length - 1; j > 0; j--) {
+                for (let i = 0; i < j; i++) {
+                  if (arr[i] > arr[i + 1]) {
+                    let temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                  }
+                }
+              }
+            }
+          `}
+        </code>
+      </pre>
+      {user.role !== 'student' && <AssessmentForm />}
     </section>
   );
 };
