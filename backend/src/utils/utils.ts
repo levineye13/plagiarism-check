e76d98474f61f497e3448aa81d6f77292f5c5b34
Oleg Zharov <levineye@gmail.com>
2022-11-23 13:19:39 +0300

    feat: добавить функции для удобной генерации jwt токенов

--- backend/src/utils/utils.ts
+++ backend/src/utils/utils.ts
@@ -1,4 +1,8 @@
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+import { SECRET_KEY } from '../config';
+import { TOKEN_TYPE } from './constants';
 
 export const getPasswordHash = (password: string, salt: number): string => {
   let passwordHash = password;
@@ -9,4 +13,19 @@ export const getPasswordHash = (password: string, salt: number): string => {
   })();
 
   return passwordHash;
+};
+
+export const generateToken = (id: number, expiresIn: string | number): string =>
+  jwt.sign(`${id}`, SECRET_KEY, { expiresIn });
+
+export const generateKeychain = (
+  id: number
+): { access: string; refresh: string } => {
+  const access = `${TOKEN_TYPE} ${generateToken(id, 1000 * 60 * 20)}`;
+  const refresh = generateToken(id, '7d');
+
+  return {
+    access,
+    refresh,
+  };
 };
