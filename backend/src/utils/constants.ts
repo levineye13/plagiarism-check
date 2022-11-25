8b839cd5a859965caffe2ac9ef94bce9b7c9d3fa
Oleg Zharov <levineye@gmail.com>
2022-11-25 17:39:36 +0300

    feat: добавить константы

--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -4,3 +4,11 @@ export enum Token {
   Access = 'access',
   Refresh = 'refresh',
 }
+
+export enum Path {
+  users = '/users',
+  user = '/users/:id',
+  signin = '/signin',
+  signup = '/signup',
+  me = '/users/me',
+}
