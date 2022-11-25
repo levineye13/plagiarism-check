ab5a4caa333e2595f4dd0cd64ecc90adde7c0af8
Oleg Zharov <levineye@gmail.com>
2022-11-25 17:40:47 +0300

    feat: добавить главные файлы приложения

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,27 @@
 import express, { Express } from 'express';
+import cookieParser from 'cookie-parser';
+
+import { postgresStart } from './db';
+import router from './routes';
 
 const app: Express = express();
 
 const port = 3000;
 const host = 'localhost';
 
-app.listen(port, host, () => {
-  console.log('started', port, host);
-});
+app.use(cookieParser());
+app.use(router);
+
+const startApp = async (): Promise<void> => {
+  try {
+    postgresStart();
+
+    app.listen(port, host, () => {
+      console.log('started', port, host);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+startApp();
--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,19 +1,15 @@
 import { Sequelize } from 'sequelize';
 
 export const sequelize: Sequelize = new Sequelize({
-  host: '127.0.0.1',
-  port: 4000,
-  username: 'root',
-  password: '12345',
+  //database: 'plagiarism_check',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '1331',
   dialect: 'postgres',
 });
 
 export const postgresStart = async (): Promise<void> => {
-  try {
-    await sequelize.authenticate();
-    await sequelize.sync({ alter: true });
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+  await sequelize.authenticate();
+  await sequelize.sync();
 };
