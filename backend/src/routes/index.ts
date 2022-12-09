14af1738eba04ac1cd9acfad9bd59de7d0fd595e
Oleg Zharov <levineye@gmail.com>
2022-12-09 13:41:41 +0300

    feat: добавить роуты

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import userRouter from './users';
+import groupRouter from './group';
 import { Path } from '../utils/constants';
 import User from '../controllers/users';
 import { auth } from '../middlewares';
@@ -11,5 +12,6 @@ router.post(Path.signin, User.login);
 router.post(Path.signup, User.register);
 router.use(auth);
 router.use(userRouter);
+router.use(groupRouter);
 
 export default router;
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -2,13 +2,13 @@ import { Router } from 'express';
 
 import User from '../controllers/users';
 import { role } from '../middlewares';
-import { Path } from '../utils/constants';
+import { Path, Role } from '../utils/constants';
 
 const router = Router();
 
 router.get(Path.users, User.getAll);
 router.get(Path.me, User.getMe);
 router.get(Path.user, User.getById);
-router.delete(Path.user, role(['admin', 'moderator']), User.delete);
+router.delete(Path.user, role([Role.Admin, Role.Moderator]), User.delete);
 
 export default router;
