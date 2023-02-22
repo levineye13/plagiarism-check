572c3a5c058504a90e82cb956efa3ce57564283c
Oleg Zharov <levineye@gmail.com>
2023-02-22 13:57:12 +0300

    feat: добавить апи класс пользователя

---
+++ frontend/src/utils/api/user.ts
@@ -0,0 +1,48 @@
+import Api from './api';
+import { IUser } from '../interfaces';
+import { IApiUser, TApiAnswer } from './types';
+import { HTTP_METHODS, HEADERS, API_ROUTES } from './constants';
+
+class UserApi extends Api {
+  public constructor(baseUrl: string, headers: { [key: string]: string } = {}) {
+    super(baseUrl, headers);
+  }
+
+  public register = async (user: IApiUser): Promise<IUser | never> => {
+    const res: Response = await fetch(`${this.baseUrl}/${API_ROUTES.signup}`, {
+      method: HTTP_METHODS.post,
+      headers: this.headers,
+      body: JSON.stringify(user),
+    });
+
+    const newUser = await this.checkResponce<IUser>(res);
+    return newUser;
+  };
+
+  public login = async (
+    user: Omit<IApiUser, 'login'>
+  ): Promise<IUser | never> => {
+    const res: Response = await fetch(`${this.baseUrl}/${API_ROUTES.signin}`, {
+      method: HTTP_METHODS.post,
+      headers: this.headers,
+      body: JSON.stringify(user),
+    });
+
+    const currentUser = await this.checkResponce<IUser>(res);
+    return currentUser;
+  };
+
+  public logout = async (): Promise<TApiAnswer | never> => {
+    const res: Response = await fetch(`${this.baseUrl}/${API_ROUTES.signout}`, {
+      method: HTTP_METHODS.head,
+      headers: HEADERS,
+    });
+
+    const data = await this.checkResponce<TApiAnswer>(res);
+    return data;
+  };
+}
+
+const apiUser = new UserApi('', HEADERS);
+
+export { apiUser };
