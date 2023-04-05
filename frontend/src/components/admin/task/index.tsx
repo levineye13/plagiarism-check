018a1a4ce53ba1e6f6a9d394e9a6e48e2d5343b0
Oleg Zharov <levineye@gmail.com>
2023-04-05 14:59:09 +0300

    feat: добавить компонент таска

---
+++ frontend/src/components/admin/task/index.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react';
+
+import Back from '../../back-button';
+import Title from '../../section-title';
+import styles from './index.module.scss';
+import { useAppSelector } from '../../../store/hooks';
+import GroupList from '../../group-list';
+import { useLocation } from 'react-router-dom';
+import { ROUTES } from '../../../utils/constants';
+
+const Task: FC = () => {
+  const { title, text, description } = useAppSelector((state) => state.task);
+  const { pathname } = useLocation();
+
+  return (
+    <section className={styles.section}>
+      <Back />
+      <Title>{title}</Title>
+      <p className={styles.description}>{description}</p>
+      <p className={styles.text}>{text}</p>
+      <GroupList path={`${pathname}/${ROUTES.groups}`} />
+    </section>
+  );
+};
+
+export default Task;
