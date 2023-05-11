b2a72454c8f352dcc064d2b4e76049b38adb742b
Oleg Zharov <levineye@gmail.com>
2023-05-11 13:23:05 +0300

    feat: переносить авторизованного

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,9 +1,17 @@
 import React, { FC } from 'react';
+import { Navigate } from 'react-router-dom';
 
 import Form from '../../components/login-form';
 import styles from './index.module.scss';
+import { useAppSelector } from '../../store/hooks';
+import { ROUTES } from '../../utils/constants';
 
 const Login: FC = () => {
+  const { isAuth } = useAppSelector((state) => state.auth);
+
+  if (isAuth) {
+    return <Navigate to={ROUTES.home} replace />;
+  }
   return (
     <section className={styles.section}>
       <Form />
--- frontend/src/pages/register/index.tsx
+++ frontend/src/pages/register/index.tsx
@@ -1,9 +1,18 @@
 import React, { FC } from 'react';
+import { Navigate } from 'react-router-dom';
 
 import Form from '../../components/register-form';
 import styles from './index.module.scss';
+import { useAppSelector } from '../../store/hooks';
+import { ROUTES } from '../../utils/constants';
 
 const Register: FC = () => {
+  const { isAuth } = useAppSelector((state) => state.auth);
+
+  if (isAuth) {
+    return <Navigate to={ROUTES.home} replace />;
+  }
+
   return (
     <section className={styles.section}>
       <Form />
