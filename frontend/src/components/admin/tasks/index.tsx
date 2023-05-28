b82d2729561d40cb99e5620b53180e273334409c
Oleg Zharov <levineye@gmail.com>
2023-05-28 15:36:17 +0300

    feat: вывести созданные задачи

--- frontend/src/components/admin/tasks/index.tsx
+++ frontend/src/components/admin/tasks/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 
 import Title from '../../section-title';
 import Back from '../../back-button';
@@ -7,24 +7,28 @@ import Task from '../../task';
 import { useLocation } from 'react-router-dom';
 import DeleteButton from '../../delete-button';
 import { useAppDispatch, useAppSelector } from '../../../store/hooks';
-import { deleteTask } from '../../../store/actions';
-import { TTask } from '../../../utils/types';
+import { deleteTask, getAllTaskOwner } from '../../../store/actions';
+import { ITask } from '../../../utils/interfaces';
 
 const Tasks: FC = () => {
   const { state } = useLocation();
-  const { tasks } = useAppSelector((state) => state.tasks);
+  const { allOwner } = useAppSelector((state) => state.tasks);
   const dispatch = useAppDispatch();
 
-  const handleDelete = (task: TTask) => {
+  const handleDelete = (task: ITask) => {
     dispatch(deleteTask(task));
   };
+
+  useEffect(() => {
+    dispatch(getAllTaskOwner());
+  }, [dispatch]);
 
   return (
     <section className={styles.section}>
       <Back />
       <Title style={{ marginBottom: 20 }}>Задания</Title>
       <ul className={styles.list}>
-        {tasks.map((task) => (
+        {allOwner.map((task) => (
           <li className={styles.item} key={task.id}>
             <DeleteButton
               className={styles.delete}
@@ -33,8 +37,8 @@ const Tasks: FC = () => {
             <Task
               taskId={task.id}
               subjectId={state?.subjectId || null}
-              title={task.title}
               description={task.description}
+              language={task.language}
             >
               {task.text}
             </Task>
